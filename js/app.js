5560ed24b4a85a7732341df30403f606c343737f
Ryan A. Muller <ryan@muller.io>
2015-01-23 23:41:36 -0500

    Started RequireJS stuff

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 // # Require.JS configuration
 requirejs.config({
   paths: {
-
+    'ampersand-model': '/bower_components/ampersand-model/ampersand-model',
+    'ampersand-router': '/bower_components/ampersand-model/ampersand-router',
+    'ampersand-view': '/bower_components/ampersand-model/ampersand-view',
+    'ampersand-state': '/bower_components/ampersand-model/ampersand-state',
+    'ampersand-sync': '/bower_components/ampersand-model/ampersand-sync'
   },
 
   shim: {
+    'ampersand-model':
   }
+});
+
+require(['./Router'], function(Router){
+
 });
