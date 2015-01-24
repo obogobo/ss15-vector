6894eb33e5450dc47d1ca630c2c4c718aef3b0a2
Jackson Westeen <jcw21@email.arizona.edu>
2015-01-24 15:27:34 -0500

    new p2p

--- js/p2p.js
+++ js/p2p.js
@@ -4,12 +4,6 @@ $.getScript('http://cdn.peerjs.com/0.3.9/peer.js')
     .success(function () {
         self = new Peer('jcw', { key: 'lwjd5qra8257b9' });
 
-        // setup own handlers
-        self.on('disconnected', function () {
-            console.log('Connection lost, reconnecting...');
-            self.reconnect();
-        });
-
         self.on('connection', function(conn) {
             conn.on('open', function() {
                 console.log('Incoming connection:', conn.peer);
@@ -24,8 +18,10 @@ $.getScript('http://cdn.peerjs.com/0.3.9/peer.js')
             })
         });
 
+        debugger;
+
         // TODO: connect to everyone else in Firebase!
-        var connection = self.connect('else');
+        var connection = self.connect('mcbex');
         connection.on('open', function () {
             console.log('Opened connection:', connection.peer);
 
