50d2f11bf3494fdc506718d8f8e2f18d4acb476b
Ryan A. Muller <ryan@muller.io>
2015-01-25 10:56:17 -0500

    Started revamp of UI

--- js/main/MainView.js
+++ js/main/MainView.js
@@ -14,12 +14,14 @@ module.exports = AmpersandView.extend({
   },
   render: function(){
     AmpersandView.prototype.render.apply(this, arguments);
-    var $column = $(this.el).find('.grid > .column').first();
-    var card = new TextChatCard({peer: this.peer});
-    $column.append(card.render().el);
+    var $el = $(this.el), $column = $el.find('.grid > .column').first();
+    this.card = this.card || new TextChatCard({peer: this.peer});
+    $column.empty().append(this.card.render().el);
     if(this.peer) {
-        $(this.queryByHook('username-display')).append(' '+this.peer.username);
+        $(this.queryByHook('username-display')).children('span').text(' '+this.peer.username);
     }
+    $el.find('.ui.dropdown').dropdown({action:'nothing'});
+    $el.find('.ui.checkbox').checkbox();
     return this;
   },
   toggleModules: function(){
