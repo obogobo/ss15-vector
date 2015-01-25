fb40ac4843419c44548e2def1df2fdaca7547b75
mcbex <ming.beckwith@gmail.com>
2015-01-25 17:29:13 -0500

    update ui - try to support multiple calls

--- js/cards/video-stream/VideoStreamCard.js
+++ js/cards/video-stream/VideoStreamCard.js
@@ -1,4 +1,5 @@
-var Card = require('../Card');
+var Card = require('../Card'),
+    _ = require('lodash');
 
 module.exports = Card.extend({
     innerTemplate: require('./VideoStreamCard.jade'),
@@ -6,10 +7,7 @@ module.exports = Card.extend({
         title: 'Video Stream',
         icon: 'comments'
     },
-    events: {
-        'click div[data-action="make-call"]': 'makeCall',
-        'click div[data-action="answer-call"]': 'answerCall'
-    },
+    calls: {},
     initialize: function(options) {
         var view = this;
 
@@ -20,10 +18,15 @@ module.exports = Card.extend({
 
         this.on('hide', function() {
             // end video stream here
+            view.stream && view.stream.close();
         });
 
         this.peer.on('call:received', function(call) {
-            view.answerCall(call);
+            if ($('#my-vidya').is(':visible')) {
+                view.answerCall(call);
+            } else {
+                call.close(view.stream);
+            }
         })
     },
 
@@ -31,33 +34,48 @@ module.exports = Card.extend({
         var view = this;
 
         navigator.getUserMedia({ video: true, audio: true }, function(stream) {
+            $('#my-vidya').prop('src', URL.createObjectURL(stream)).prop('muted', true);
             view.stream = stream;
-            $('#my-vidya').prop('src', URL.createObjectURL(stream));
+            view.makeCalls();
         }, function(err) {
             console.log('Failed to get local stream' ,err);
         });
     },
 
-    makeCall: function(e) {
-        var receiver = 'mcbex1',
-            call = this.peer.socket.call(receiver, this.stream);
-
-        $(e.target).text('Calling ' + receiver + '...');
+    bindCallEvents: function(call) {
+        var $video = $('<video id="' + call.peer + '" autoplay>'),
+            view = this;
         call.on('stream', function(remoteStream) {
             // Show stream in some video/canvas element.
-            $('#ur-vidya').prop('src', URL.createObjectURL(remoteStream));
+            if (!view.calls[call.peer]) {
+                view.calls[call.peer] = call;
+                $video.prop('src', URL.createObjectURL(remoteStream)).insertAfter('#my-vidya');
+            }
+        });
+
+        call.on('close', function() {
+            debugger;
+        });
+    },
+
+    makeCalls: function() {
+        var receivers = this.peer.getPeers(),
+            view = this;
+
+        _.forEach(receivers, function(r) {
+            if (!view.calls[r]) {
+                view.bindCallEvents(view.peer.socket.call(r, view.stream));
+            }
         });
     },
 
     answerCall: function(call) {
-        $('div.pink').hide();
-        $('div.teal').show();
+        var view = this;
         navigator.getUserMedia({ video: true, audio: true }, function(stream) {
-            call.answer(stream); // Answer the call with an A/V stream.
-            call.on('stream', function(remoteStream) {
-                // Show stream in some video/canvas element.
-                $('#ur-vidya').prop('src', URL.createObjectURL(remoteStream));
-            });
+            if (!view.calls[call.peer]) {
+                call.answer(stream); // Answer the call with an A/V stream.
+                view.bindCallEvents(call);
+            }
         }, function(err) {
             console.log('Failed to get local stream' ,err);
         });
