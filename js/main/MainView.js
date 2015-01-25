f20b038b0865195617614ec91a2a19ab31915229
mcbex <ming.beckwith@gmail.com>
2015-01-25 14:51:17 -0500

    fancier ui

--- js/main/MainView.js
+++ js/main/MainView.js
@@ -12,17 +12,18 @@ module.exports = AmpersandView.extend({
   },
   events: {
     'click [data-action="toggle-modules"]': 'toggleModules',
-    'click [data-action="load-view"]': 'loadView'
-  },
-  cards: {
-    textchat: TextChatCard,
-    videostream: VideoStreamCard
+    'click [data-action="toggle-view"]': 'toggleView'
   },
   render: function(){
     AmpersandView.prototype.render.apply(this, arguments);
-    var $el = $(this.el), $column = $el.find('.grid > .column').first();
-    this.card = this.card || new TextChatCard({peer: this.peer});
-    $column.empty().append(this.card.render().el);
+    var $el = $(this.el), $textColumn = $el.find('#textchat'),
+        $videoColumn = $el.find('#videostream');
+
+    this.textchat = new TextChatCard({ peer: this.peer });
+    this.videostream = new VideoStreamCard({ peer: this.peer });
+    $textColumn.append(this.textchat.render().el);
+    $videoColumn.append(this.videostream.render().el);
+
     if(this.peer) {
         $(this.queryByHook('username-display')).children('span').text(' '+this.peer.username);
     }
@@ -39,11 +40,27 @@ module.exports = AmpersandView.extend({
       $submenu.slideDown();
     }
   },
-  loadView: function(e) {
-      var view = $(e.target).data('view');
-
-      this.card = new this.cards[view]({ peer: this.peer });
-      this.render();
-      this.card.trigger('rendered');
+  getNumVisibleViews: function() {
+    return $(this.el).find('.card-container:visible').length;
+  },
+  renderFullScreen: function() {
+    $(this.el).find('.card-container:visible').removeClass('six ten wide').addClass('sixteen wide');
+  },
+  toggleView: function(e) {
+      var $menuItem = $(e.target),
+          viewId = $menuItem.data('view'),
+          $view = $(this.el).find('#' + viewId);
+      if ($view.is(':visible')) {
+          if (this.getNumVisibleViews() == 1) {
+              return;
+          }
+          $view.removeClass('six ten sixteen wide').hide();
+          this.renderFullScreen();
+          this[viewId].trigger('hide');
+      } else {
+          $(this.el).find('#videostream').removeClass('sixteen wide').addClass('ten wide').show();
+          $(this.el).find('#textchat').removeClass('sixteen wide').addClass('six wide').show();
+          this[viewId].trigger('show');
+      }
   }
 });
